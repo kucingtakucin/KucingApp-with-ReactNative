fbb22a0d729d37744958f005791f571ec3291071
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-05-06 20:46:05 +0700

    Bikin gambar kucing hehe

--- App.js
+++ App.js
@@ -7,18 +7,27 @@
  */
 
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextInput, View, Image, ScrollView } from 'react-native';
 
 export default class App extends Component {
     render() {
         return (
-            <View style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
-                <Text>Hello, world!</Text>
-                <Text>Namaku Adam, biasa dipanggil Arthur</Text>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <View style={{padding: 10, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={{fontWeight: 'bold'}}>Hello, World!</Text>
+                    <Text>Hai, namaku Adam, biasa dipanggil Arthur!</Text>
+                </View>
+                <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}}
+                           style={{width: 200, height: 200}}
+                    />
+                    <TextInput style={{
+                        height: 40,
+                        width: 250,
+                        borderColor: 'gray',
+                        borderWidth: 1,
+                    }} defaultValue="Halo semuanya!"/>
+                </View>
             </View>
         );
     }
